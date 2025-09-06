f3eecc4eaa840187c6551ca8ee35c770a8c53dfa
Seyi Adeniyi <adeniyiemmanuel042@gmail.com>
2025-09-06 21:39:36 +0100

    Initial commit: JobSphere project setup

--- app/(frontpage)/jobs/page.js
+++ app/(frontpage)/jobs/page.js
@@ -1,37 +1,13 @@
-"use client";
-import AllJobs from "@/components/_job-categories/all-jobs";
-import ContractJobs from "@/components/_job-categories/contract-job";
-import InternshipJobs from "@/components/_job-categories/internship-jobs";
-import RemoteJobs from "@/components/_job-categories/remote-jobs";
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
-import { useSearchParams } from "next/navigation";
-import React from "react";
+import JobsPage from '@/components/_frontpage/jobs-page'
+import { JobApplicationSkeleton } from '@/components/_skeleton/job-card-skeleton'
+import React, { Suspense } from 'react'
 
-export default function JobsPage() {
-  const searchParams = useSearchParams();
-  const remote = searchParams.get("remote");
-  const contract = searchParams.get("contract");
-  const internship = searchParams.get("internship");
-
-  const { data, isLoading, isError, error } = useQuery({
-    queryKey: ["jobs", { remote, contract, internship }],
-    queryFn: async () => {
-      const res = await axios.get(
-        `${process.env.NEXT_PUBLIC_BACKEND_URL}/jobs/public-jobs`
-      );
-      return res.data.data;
-    },
-    staleTime: 5 * 60 * 1000, // cache for 5 mins
-  });
-
- 
-
-
-  let Component = AllJobs;
-  if (searchParams.toString().includes("remote")) Component = RemoteJobs;
-  else if (searchParams.toString().includes("contract")) Component = ContractJobs;
-  else if (searchParams.toString().includes("internship")) Component = InternshipJobs;
-
-  return <Component data={data} isLoading={isLoading} />;
+export default function page() {
+  return (
+    <div> 
+      <Suspense fallback={<div><JobApplicationSkeleton/></div>}>
+      <JobsPage/>
+    </Suspense>
+    </div>
+  )
 }
